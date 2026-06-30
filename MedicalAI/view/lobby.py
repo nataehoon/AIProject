7ece1a98eb8eec9ff630ff32b5bdcc8b008b293e
hoon's <skeoddla@gmail.com>
2026-06-30 00:24:13 +0000

    status UI

--- MedicalAI/view/lobby.py
+++ MedicalAI/view/lobby.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from models.member import UserSessionDTO
+#from models.member import UserSessionDTO
 
 st.set_page_config(
     page_title="MRI Parsing Engine",
@@ -7,20 +7,47 @@ st.set_page_config(
     layout="wide"
 )
 
+if "status" not in st.session_state:
+    st.session_state.status = 0
+
 one_row_height=500
 
 col1, col2 = st.columns(2)
+with col2:
+    status_zone = st.empty()
+
+def rerander_status():
+    with status_zone:
+        with st.container():
+            st.header("Status")
+            with st.container(height=one_row_height, border=True):
+                if st.session_state.status == 0:
+                    st.markdown("1번 진행중")
+                    st.markdown("2번 진행중")
+                    st.markdown("3번 진행중")
+                if st.session_state.status == 1:
+                    st.markdown(":red[1번 진행중]")
+                    st.markdown("2번 진행중")
+                    st.markdown("3번 진행중")
+                if st.session_state.status == 2:
+                    st.markdown("1번 진행중")
+                    st.markdown(":red[2번 진행중]")
+                    st.markdown("3번 진행중")
+                if st.session_state.status == 3:
+                    st.markdown("1번 진행중")
+                    st.markdown("2번 진행중")
+                    st.markdown(":red[3번 진행중]")
+
+rerander_status()
+
 with col1:
     st.header("Searched Paper")
     paper_placeholder = st.empty()
     with paper_placeholder:
         with st.container(height=one_row_height, border=True):
-            st.button("🔍 새 자료 찾기")
-with col2:
-    st.header("Status")
-    status_placeholder = st.empty()
-    with status_placeholder:
-        with st.container(height=one_row_height, border=True):
-            st.markdown(":red[1번 진행중]")
-            st.markdown("2번 진행중")
-            st.markdown("3번 진행중")
+            search = st.button("🔍 새 자료 찾기")
+            if search:
+                st.session_state.status += 1
+                if st.session_state.status == 4:
+                    st.session_state.status = 0
+                rerander_status()
