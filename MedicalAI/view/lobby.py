e013a043479637dc116926b508d26df84246b6bc
hoon's <skeoddla@gmail.com>
2026-06-30 12:17:44 +0000

    ux

--- MedicalAI/view/lobby.py
+++ MedicalAI/view/lobby.py
@@ -11,6 +11,17 @@ st.set_page_config(
 if "status" not in st.session_state:
     st.session_state.status = 0
 
+if "searching" not in st.session_state:
+    st.session_state.searching = False
+
+status_text = [
+    "1. 허깅 페이스에서 자료 다운로드 중...",
+    "2. 텍스트 청킹 및 전처리 중...",
+    "3. 임베딩 모델 로드 후 백터화 중...",
+    "4. 벡터 데이터베이스 저장 및 인덱싱 중...",
+    "5. RAG시스템 준비 완료!"
+]
+
 one_row_height=500
 
 col1, col2 = st.columns(2)
@@ -23,26 +34,41 @@ def rerander_status():
             st.header("Status")
             with st.container(height=one_row_height, border=True):
                 if st.session_state.status == 0:
-                    st.markdown("1번 진행중")
-                    st.markdown("2번 진행중")
-                    st.markdown("3번 진행중")
+                    st.markdown(f"{status_text[0]}")
+                    st.markdown(f"{status_text[1]}")
+                    st.markdown(f"{status_text[2]}")
+                    st.markdown(f"{status_text[3]}")
+                    st.markdown(f"{status_text[4]}")
                 if st.session_state.status == 1:
-                    st.markdown(":red[1번 진행중]")
-                    st.markdown("2번 진행중")
-                    st.markdown("3번 진행중")
+                    st.markdown(f":green[{status_text[0]}]")
+                    st.markdown(f"{status_text[1]}")
+                    st.markdown(f"{status_text[2]}")
+                    st.markdown(f"{status_text[3]}")
+                    st.markdown(f"{status_text[4]}")
                 if st.session_state.status == 2:
-                    st.markdown("1번 진행중")
-                    st.markdown(":red[2번 진행중]")
-                    st.markdown("3번 진행중")
+                    st.markdown(f"{status_text[0]}")
+                    st.markdown(f":green[{status_text[1]}]")
+                    st.markdown(f"{status_text[2]}")
+                    st.markdown(f"{status_text[3]}")
+                    st.markdown(f"{status_text[4]}")
                 if st.session_state.status == 3:
-                    st.markdown("1번 진행중")
-                    st.markdown("2번 진행중")
-                    st.markdown(":red[3번 진행중]")
+                    st.markdown(f"{status_text[0]}")
+                    st.markdown(f"{status_text[1]}")
+                    st.markdown(f":green[{status_text[2]}]")
+                    st.markdown(f"{status_text[3]}")
+                    st.markdown(f"{status_text[4]}")
                 if st.session_state.status == 4:
-                    st.markdown("1번 진행중")
-                    st.markdown("2번 진행중")
-                    st.markdown("3번 진행중")
-                    st.markdown(":green[완료]")
+                    st.markdown(f"{status_text[0]}")
+                    st.markdown(f"{status_text[1]}")
+                    st.markdown(f"{status_text[2]}")
+                    st.markdown(f":green[{status_text[3]}]")
+                    st.markdown(f"{status_text[4]}")
+                if st.session_state.status == 5:
+                    st.markdown(f"{status_text[0]}")
+                    st.markdown(f"{status_text[1]}")
+                    st.markdown(f"{status_text[2]}")
+                    st.markdown(f"{status_text[3]}")
+                    st.markdown(f":green[{status_text[4]}]")
 
 rerander_status()
 
@@ -51,20 +77,30 @@ with col1:
     paper_placeholder = st.empty()
     with paper_placeholder:
         with st.container(height=one_row_height, border=True):
-            search = st.button("🔍 새 자료 찾기")
-            if search:
+            search = st.button("🔍 새 자료 찾기", disabled=st.session_state.searching)
+            if search or st.session_state.searching == True:
+                if st.session_state.searching == False:
+                    st.session_state.searching = True
+                    st.rerun()
                 st.session_state.status = 0
                 st.session_state.status += 1
                 rerander_status()
-                time.sleep(5)
+                time.sleep(2)
 
                 st.session_state.status += 1
                 rerander_status()
-                time.sleep(5)
+                time.sleep(2)
 
                 st.session_state.status += 1
                 rerander_status()
-                time.sleep(5)
+                time.sleep(2)
 
                 st.session_state.status += 1
                 rerander_status()
+                time.sleep(2)
+
+                st.session_state.status += 1
+                rerander_status()
+
+                st.session_state.searching = False
+                st.rerun()
